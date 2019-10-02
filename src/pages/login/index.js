bb372489df2a3ef13109c60ff735124afa3f84f9
cycyjing <cycycyjing@gmail.com>
2019-10-02 23:10:15 +1300

    02/10/2019-finish admin login form

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,19 +1,67 @@
 import React, { Component } from 'react'
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import './login.less'
 import logo from '../../images/login/logo.png'
-export default class Login extends Component {
+class Login extends Component {
+  handleSubmit = e => {
+    const { getFieldsValue } = this.props.form
+    e.preventDefault();
+    const params = getFieldsValue()
+    console.log(params)
+  };
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
       <div className='login'>
         <header className='login-header'>
-          <img src={logo} alt=''/>
-          <h1>React Project</h1>
+          <img src={logo} alt='' />
+          <h1>React Project: Management System</h1>
         </header>
         <section className='login-content'>
-          <header>Login</header>
-          FORM 
+          <h2>Log in</h2>
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <Form.Item>
+              {getFieldDecorator('username', {
+                rules: [
+                  {required:true,message:'Please input username'},
+                  {min:4,message:'Please input username at least 4 characters'},
+                  {max:12,message:'Please input username no more than 12 characters'},
+                  {pattern:/^[a-zA-Z0-9]+$/,message:'Please input username with letter, number and under dash'},
+                ]
+              })(
+                <Input
+                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  placeholder="Username"
+                />
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('password',{
+                rules:[
+                  {required:true,message:'Please input password'},
+                  {min:6,message:'Please input password at least 6 characters'},
+                  {max:20,message:'Please input password no more than 20 characters'},
+                ]
+              })(
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  type="password"
+                  placeholder="Password"
+                />
+              )}
+            </Form.Item>
+            <Form.Item>
+
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+
         </section>
-      </div> 
+      </div>
     )
   }
 }
+const WrapLogin = Form.create()(Login)
+export default WrapLogin
